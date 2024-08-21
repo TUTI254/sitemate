4ef5fbb3c52093c90fc22c026289e8216110e356
CT254 <t_collins@robotconsulting.net>
2024-08-21 23:16:09 +0300

    chore: refactor

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { QueryClientProvider } from '@tanstack/react-query';
-import queryClient from './utils/queryClient.js';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
       <App />
-    </QueryClientProvider>
   </StrictMode>,
 )
